d41ab9c528e54b9418cb00657887a84d4451f747
Alfonso E. Romero <alfonsoeromero@gmail.com>
2013-12-28 01:11:47 +0000

    Revert "yeah"
    This reverts commit 83c9c26af2f2d66b3605978fc3828a77812a122d.

--- build/lib.linux-x86_64-2.7/gfam/compat/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Compatibility classes for Python 2.5 and earlier."""
-
-from gfam.compat.namedtuple import namedtuple
-from gfam.compat.abc import Mapping
-
-__all__ = ["Mapping", "namedtuple"]
-
