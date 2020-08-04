59bcb689bdfc7632c5e1c5b48eb149d73889c97a
Alfonso E. Romero <alfonsoeromero@gmail.com>
2020-08-04 20:31:13 +0200

    Add integration test for assignment source filter

---
+++ test/fixtures/assignment_source_filter_fixture.py
@@ -0,0 +1,32 @@
+import os
+from typing import List
+
+
+class AssignmentSourceFilterFixture:
+    def __init__(self):
+        current_dir = os.path.dirname(__file__)
+        self.data_dir = os.path.join(current_dir, os.pardir, "data")
+
+    def get_default_args_for_app(self) -> List[str]:
+        assignment_file: str = self.get_input_assignment_file()
+        interpro_parent_child_file: str = self.get_parent_child_interpro_file()
+
+        args = [assignment_file,
+                "-x", "HAMAP PatternScan FPrintScan Seg Coil",
+                "-e", "1e-3;superfamily=inf;HMMPanther=inf;Gene3D=inf;HMMPIR=inf",
+                "-i", interpro_parent_child_file,
+                "--max-overlap", "20"]
+        return args
+
+    def get_expected_assignment_file_as_string(self) -> str:
+        output_file = os.path.join(
+            self.data_dir, "assignment_source_filter100.txt")
+        with open(output_file, "r") as f_in:
+            out_str = f_in.read()
+        return out_str
+
+    def get_input_assignment_file(self) -> str:
+        return os.path.join(self.data_dir, "uniprot_trembl.interpro")
+
+    def get_parent_child_interpro_file(self) -> str:
+        return os.path.join(self.data_dir, "ParentChildTreeFile.txt")
---
+++ test/integration/test_assignment_source_filter.py
@@ -0,0 +1,39 @@
+import io
+import unittest
+from contextlib import redirect_stdout
+from test.fixtures.assignment_source_filter_fixture import AssignmentSourceFilterFixture
+from typing import List
+
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
+from gfam.scripts.assignment_source_filter import AssignmentSourceFilterApp
+
+
+class TestAssignmentSourceFilter(unittest.TestCase):
+    def setUp(self):
+        self.fixture = AssignmentSourceFilterFixture()
+
+    def _get_obtained_output(self, sut: AssignmentSourceFilterApp,
+                             args: List[str]) -> str:
+        """Runs the given `AssignmentSourceFilterApp` `sut` with the
+        provided list of arguments `args` returning the output
+        as a string"""
+        output = io.StringIO()
+        # obtain output from find_unassigned and print it to a
+        # string stream
+        with redirect_stdout(output):
+            sut.run(args=args)
+        return output.getvalue()
+
+    def test_output_should_match_expected(self):
+        # arrange
+        args = self.fixture.get_default_args_for_app()
+        expected_output = self.fixture.get_expected_assignment_file_as_string()
+        _sut = AssignmentSourceFilterApp()
+
+        # act
+        obtained_output = self._get_obtained_output(_sut, args)
+
+        # assert
+        self.assertEqual(obtained_output, expected_output)
